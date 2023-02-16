d77c2215cbedbc5fb302904d21f5d4b0ae659ab8
Albina Lutfullina <106741510+albinalutfullina@users.noreply.github.com>
2023-02-16 15:58:21 +0500

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Альбина on 16.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
